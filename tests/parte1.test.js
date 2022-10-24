56cf9eca80ad18a1ab23797a6418923c166b1966
Isoldios <isoldi772@gmail.com>
2022-10-23 21:45:02 -0300

    TDD iteration

--- tests/parte1.test.js
+++ tests/parte1.test.js
@@ -11,3 +11,9 @@ test("Cuantas luces hay encendidas luego de encender 9",()=>{
     expect(letrero.totalEncendidas()).toBe(9);
 });
 
+test("Cuantas luces hay encendidas luego de encender 9",()=>{
+    const letrero = new Letrero();
+    letrero.encenderLuces([15,15],[13,13]);
+    expect(letrero.totalEncendidas()).toBe(9);
+});
+
