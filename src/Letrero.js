505b37c89ef516794d7f6faa5b13187b45ef9251
Isoldios <isoldi772@gmail.com>
2022-10-23 22:10:13 -0300

    TDD iteration

--- src/Letrero.js
+++ src/Letrero.js
@@ -2,7 +2,17 @@ function Letrero(){
     this.totalEncendidas= function(){
         return 0;
     }
+    this.ordenarCoordenadas=function(cord1,cord2){
+        if (cord1[0]>cord2[0] || cord1[1]>cord2[1])
+            var aux0 =cord1[0];
+            var aux1 =cord1[1];
+            cord1[0]=cord2[0];
+            cord1[1]=cord2[1];
+            cord2[0]=aux0;
+            cord2[1]=aux1; 
+    }
     this.encenderLuces = function(cord1, cord2){
+        this.ordenarCoordenadas(cord1,cord2)
         var cantidad = (cord2[0]-cord1[0]+1)*(cord2[1]-cord1[1]+1);
         this.totalEncendidas= function(){
             return cantidad;
